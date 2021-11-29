12e243429056e73ba5d3f71eabf0a4bd7daa4b37
Urvish <ubundela55@gmail.com>
2021-11-30 02:44:20 +0530

    Initial commit

---
+++ app/src/main/java/isomora/com/greendoctor/AskActivity.kt
@@ -0,0 +1,11 @@
+package isomora.com.greendoctor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AskActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ask)
+    }
+}
